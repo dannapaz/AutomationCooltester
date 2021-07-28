012202ccc255d4e598f2209b80a6622553b13a65
Danna Paz <Danna Paz@DESKTOP-72LP4JV>
2021-07-27 23:04:52 -0500

    Ejercicio 1

---
+++ AutomationCooltesters/src/test/java/com/java/Multiplicacion.java
@@ -0,0 +1,14 @@
+package com.java;
+
+public class Multiplicacion {
+
+	public static void main(String[] args) {
+		int val=3;
+		int val2=2;
+		int val3=5;
+		
+		System.out.println("La multiplicación de los 3 números es: " + val*val2*val3);
+
+	}
+
+}
